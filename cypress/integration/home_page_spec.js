652173c0aefada8c5cedaf19247e44c55169d5f0
sotowang <sotowang@qq.com>
2019-06-07 16:30:21 +0800

    backstop test finished

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -10,13 +10,17 @@ describe('Todo App Test', function() {
     cy.get('input').type('任务3');
     cy.contains('Add').click();
 
+    cy.pause(1000);
+
+
     cy.get('.delete-btn').first().click();
 
     cy.contains('Edit').first().click();
     cy.get('#app > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > form > input').clear()
         .type("修改任务1");
-
     cy.get('button').contains('Save').click();
+
+    cy.pause(1000);
 
     cy.contains('修改任务1').click();
 
