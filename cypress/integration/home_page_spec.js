d11945be940d5f9b984f1dccedfccc06e0caccb3
sotowang <sotowang@qq.com>
2019-06-07 14:39:39 +0800

    cypress test finished

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,15 +1,24 @@
 describe('Todo App Test', function() {
-  it('successfully', function() {
-    cy.visit('https://localhost:8080');
+  it('successfully loads', function() {
+    cy.visit('/');
+    cy.get('input').type('任务1');
+    cy.contains('Add').click();
 
-    cy.pause();
+    cy.get('input').type('任务2');
+    cy.contains('Add').click();
 
-    cy.contains('type').click();
+    cy.get('input').type('任务3');
+    cy.contains('Add').click();
 
-    cy.url().should('include', 'commands/actions');
+    cy.get('.delete-btn').first().click();
 
-    cy.get('.action-email')
-        .type('sotowang@qq.com')
-        .should('have.value', 'sotowang@qq.com');
+    cy.contains('Edit').first().click();
+    cy.get('#app > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > form > input').clear()
+        .type("修改任务1");
+
+    cy.get('button').contains('Save').click();
+
+    cy.contains('修改任务1').click();
+
   })
 })
